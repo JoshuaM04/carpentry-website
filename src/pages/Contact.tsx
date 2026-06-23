59721e340a20bbcb05290eee8c3ab9faf9e9f805
Viper <pruncle21@gmail.com>
2026-06-23 12:58:44 -0500

    Fixed the labels for the input fields in Contact.tsx, so they are properly associated with the correct id.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,27 +7,27 @@ export default function Contact() {
                     <h2 className="text-4xl font-semibold">Contact Us</h2>              
                 </div>
 
-                <form className="flex flex-col items-start gap-10 mb-20">
+                <form action="https://formspree.io/f/{form_id}" method="post" className="flex flex-col items-start gap-10 mb-20">
                     <div className="flex flex-wrap justify-between gap-10 w-full">
                         <div className="input-container flex flex-col gap-2">
-                            <label>Name</label>
-                            <input className="border border-slate-400 p-2 w-full" type="name" name="full-name" placeholder="Name" />
+                            <label htmlFor="name">Name</label>
+                            <input id="name" className="border border-slate-400 p-2 w-full" type="name" name="full-name" placeholder="Name" />
                         </div>
 
                         <div className="input-container flex flex-col gap-2">
-                            <label>Email*</label>
-                            <input className="border border-slate-400 p-2 w-full" type="email" name="email" placeholder="Email" required />
+                            <label htmlFor="email">Email*</label>
+                            <input id="email" className="border border-slate-400 p-2 w-full" type="email" name="email" placeholder="Email" required />
                         </div>
                     </div>
 
                     <div className="flex flex-col gap-2 w-full">
-                        <label>Phone number</label>
-                        <input className="border border-slate-400 w-full p-2" type="email" name="phone-number" placeholder="Phone number" />
+                        <label htmlFor="phone-number">Phone number</label>
+                        <input id="phone-number" className="border border-slate-400 w-full p-2" type="text" name="phone-number" placeholder="Phone number" />
                     </div>
 
                     <div className="flex flex-col gap-2 w-full">
-                        <label>Message*</label>
-                        <textarea className="border border-slate-400 w-full p-2 resize-none h-100" name="email" placeholder="Message" required />
+                        <label htmlFor="message">Message*</label>
+                        <textarea id="message" className="border border-slate-400 w-full p-2 resize-none h-100" name="message" placeholder="Message" required />
                     </div>
                     
                     <button type="submit" className="font-semibold border-2 border-slate-200 bg-slate-200 rounded-md pt-2 pb-2 pl-4 pr-4 hover:cursor-pointer hover:border-slate-900">Submit</button>
