ab550ab3feaf61f5a69fa6b20a983c3aa1baaae7
Viper <pruncle21@gmail.com>
2026-06-23 15:41:23 -0500

    Added the font style to the modal dialog since it is a layer above the DOM.

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -62,7 +62,7 @@ export default function navigation() {
                             <svg className="w-10 stroke-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 7L4 7" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 12L4 12" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 17L4 17" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
                         </Button>
 
-                        <Modal className="z-2 bg-black fixed left-[50%] top-[50%] translate-[-50%] w-full h-full p-10">
+                        <Modal className="z-2 bg-black fixed left-[50%] top-[50%] translate-[-50%] w-full h-full p-10 font-roboto">
                             <Dialog className="flex flex-col gap-20">
                                 <div className="flex justify-between">
                                     <Heading className="text-white uppercase flex flex-col items-center text-xl font-bold border-t-4 border-b-4 pt-2 pb-2 pl-6 pr-6">
