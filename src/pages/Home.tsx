7990bfe268aeeaf1daa64c18c8d04ea78f444df1
Viper <pruncle21@gmail.com>
2026-06-23 15:50:36 -0500

    Made a small vertical positioning adjustment to the h1 container in the hero section, so it remains at a lower position on mobile resolutions.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
                     <div className="h-screen w-full overflow-hidden mt-40 pointer-events-none"><img className="top-0 left-0 w-full h-full object-cover" src="/heroSectionImage.avif" alt="" /></div>
 
-                    <div className="text-white top-[45%] left-[50%] translate-x-[-50%] absolute max-2md:w-56.25">
+                    <div className="text-white top-[45%] left-[50%] translate-x-[-50%] absolute max-2md:w-56.25 max-2md:top-[50%]">
                         <h1 className="font-bold flex flex-col gap-4 text-6xl text-center">
                             <div>Hand Crafted</div>
                             <div>Real Materials</div>
