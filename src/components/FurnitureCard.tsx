ceb89e8f99e2b88c08b2155e82852228a1f4f35c
Viper <pruncle21@gmail.com>
2026-07-09 18:43:29 -0500

    Made some styling adjustments to the FurnitureCard component, so the content appears evenly spaced with a border around the text content.

--- src/components/FurnitureCard.tsx
+++ src/components/FurnitureCard.tsx
@@ -36,26 +36,28 @@ export default function FurnitureCard({ product }: FurnitureCardProps) {
     return (
         <div className="flex flex-wrap items-center gap-10 max-xsm:justify-center">
             <Link to={product.route} className="flex flex-col gap-5 w-50 h-fit">
-                <img className="size-50" src={product.image} alt={product.name} />
+                <img className="h-50" src={product.image} alt={product.name} />
 
-                <div className="flex flex-col gap-5">
-                        <div>
-                            <p className="font-semibold">{product.name}</p>
-                            <div>
-                                <p className={`${data.length === 0 ? 'hidden' : 'block'}`}>{averageRating.toFixed(1)} / 5.0</p>
-                                <p className={`${data.length === 0 ? 'block' : 'hidden'}`}>No reviews yet</p>
-                            </div>
-                        </div>
-                    
-                        <div>
-                            <div className="flex gap-2">
-                                <div className="w-4 h-4 bg-black"></div>
-                                <div className="w-4 h-4 bg-red-900"></div>
-                            </div>
-                            <p className="text-xs">2 color options</p>
+                <div className="flex flex-col gap-5 p-5 -mt-5 border-2">
+                    <div>
+                        <p className="text-lg font-semibold">{product.name}</p>
+                        <p>${product.price}</p>
+                    </div>
+                
+                    <div className="flex flex-col gap-2">
+                        <div className="flex gap-2">
+                            <div className="w-8 h-5 bg-black"></div>
+                            <div className="w-8 h-5 bg-red-900"></div>
                         </div>
 
-                    <p className="text-xs italic">Approximately <span className="font-bold">2 days</span> completion</p>
+                        <p className="text-xs">2 color options</p>
+                    </div>
+
+                    <div>
+                        <p className={`${data.length === 0 ? 'hidden' : 'block'}`}>{averageRating.toFixed(1)} / 5.0</p>
+                        <p className={`${data.length === 0 ? 'block' : 'hidden'}`}>No reviews yet</p>
+                    </div>
+                    <p className="text-xs italic">Approximately <span className="font-bold">7 days</span> completion</p>
                 </div>
             </Link>
         </div>
