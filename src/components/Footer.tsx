b60e1a9952ae1ffe16fa82134b5e585260a49f16
Viper <pruncle21@gmail.com>
2026-07-19 21:12:32 -0500

    Made further styling changes and added my socials in About.tsx.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export default function footer() {
     return (
-        <footer className="flex justify-center items-center text-gray-400 bg-black p-10 w-full">
-            <p className="text-xs uppercase flex gap-2">
-                woodwork creations
+        <footer className="flex flex-col justify-between items-center gap-5 text-gray-400 bg-black p-10 w-full relative">
+            <div className="text-xs uppercase flex gap-2">
+                <p>woodwork creations</p>
                 <svg className="w-3 stroke-gray-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12Z" strokeWidth="1.5"></path> <path d="M14 15.6672C13.475 15.8812 12.8952 16 12.2857 16C9.91878 16 8 14.2091 8 12C8 9.79086 9.91878 8 12.2857 8C12.8952 8 13.475 8.11876 14 8.33283" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg>
-                2026
-            </p>
+                <p>2026</p>
+            </div>
         </footer>
     )
 }
