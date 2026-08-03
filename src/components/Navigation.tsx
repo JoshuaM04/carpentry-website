2943856068dcf9570f9d401c44e5317a69e4dd35
Viper <pruncle21@gmail.com>
2026-08-02 22:25:24 -0500

    Added a breakpoint to the website title so it appears centered on desktop resolutions and left-aligned on mobile resolutions.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,7 +29,7 @@ export default function Navigation({ cart, setCart }: NavigationProps) {
     }, []);
 
     const brand = (
-        <div className="flex flex-col leading-none">
+        <div className="flex flex-col leading-none 2md:items-center">
             <p className="display text-[0.8125rem] tracking-[0.3em]">WoodWork</p>
             <p className="display text-[0.8125rem] tracking-[0.3em]">Creations</p>
         </div>
